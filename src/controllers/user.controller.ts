b984df0decf6f34decab9440bf12cf2f30a42d5e
raghusanke <raghusanke49@gmail.com>
2026-01-07 18:29:18 +0530

    Refactor user controller to include next() for errors
    Updated user controller methods to use next() for error handling.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,33 +2,33 @@ import { Request, Response } from "express";
 import * as userService from "../services/user.service";
 import logger from "../utils/logger.ts";
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
       logger.info(`Register request: ${req.body.email}`);
     const user = await userService.registerUser(req.body);
     res.status(201).json({ success: true, user });
   } catch (err: any) {
-     logger.error(`Register failed: ${err.message}`);
-    res.status(400).json({ success: false, message: err.message });
+    logger.error(`Register failed: ${err.message}`);
+    next();
   }
 };
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
     const result = await userService.loginUser(email, password);
     res.status(200).json({ success: true, ...result });
   } catch (err: any) {
-    res.status(400).json({ success: false, message: err.message });
+     next();
   }
 };
 
-export const forgotPassword = async (req: Request, res: Response) => {
+export const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email } = req.body;
     const result = await userService.forgotPassword(email);
     res.status(200).json({ success: true, ...result });
   } catch (err: any) {
-    res.status(400).json({ success: false, message: err.message });
+     next();
   }
 };
